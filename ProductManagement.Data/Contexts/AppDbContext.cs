a33145625974b65ddaeaa38438bed6f74261bdcf
CS <azimochilov226@gmail.com>
2023-07-15 11:47:38 +0300

    data layer is done

---
+++ ProductManagement.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Domain.Entities;
+
+namespace ProductManagement.Data.Contexts;
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
---
+++ ProductManagement.Data/Repositories/Repository.cs
@@ -0,0 +1,59 @@
+﻿using ProductManagement.Data.IRepositories;
+using ProductManagement.Domain.Commons;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System;
+using ProductManagement.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagement.Data.Repositories;
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
+{
+    protected readonly AppDbContext dbContext;
+    protected readonly DbSet<TEntity> dbSet;
+    public Repository(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+        this.dbSet = dbContext.Set<TEntity>();
+    }
+
+    public async ValueTask<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        var entity = await this.SelectAsync(expression);
+
+        if (entity is not null)
+        {
+            entity.IsDeleted = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null, string[] includes = null)
+    {
+        IQueryable<TEntity> query = expression is null ? this.dbSet : this.dbSet.Where(expression);
+
+        if (includes is not null)
+        {
+            foreach (string include in includes)
+            {
+                query = query.Include(include);
+            }
+        }
+
+        return query;
+    }
+
+    public async ValueTask SaveAsync()
+        => await dbContext.SaveChangesAsync();
+
+    public async ValueTask<TEntity> InsertAsync(TEntity entity)
+        => (await this.dbSet.AddAsync(entity)).Entity;
+
+    public async ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null)
+        => await this.SelectAll(expression, includes).FirstOrDefaultAsync(t => !t.IsDeleted);
+
+    public TEntity Update(TEntity entity)
+        => (this.dbContext.Update(entity)).Entity;
+}
