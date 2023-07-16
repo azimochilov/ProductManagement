f237ced6b173ec08526922f81f0212a388544bef
CS <azimochilov226@gmail.com>
2023-07-16 22:21:14 +0300

    program.cs is done

--- ProductManagement.Web/Program.cs
+++ ProductManagement.Web/Program.cs
@@ -1,9 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Data.Contexts;
+using ProductManagement.Data.IRepositories;
+using ProductManagement.Data.Repositories;
+using ProductManagement.Service.Interfaces;
+using ProductManagement.Service.Mappers;
+using ProductManagement.Service.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductCategoryService, ProductCategoryService>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddAutoMapper(typeof(MapperProfile));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
